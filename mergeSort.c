2dabf5a3ef283c9964ba0d015ac1c29e2f14f6cf
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2023-11-15 17:49:56 +0800

    finish merge sort

--- mergeSort.c
+++ mergeSort.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-
+#include <pthread.h>
 
 void printArray(int arr[], int n){
   printf("{");
@@ -19,24 +19,50 @@ void merge(int arr[], int start, int middle, int end){
   int L[LeftSize];
   int R[RightSize];
 
-  for(i = 0; i < LeftSize; ++i)
+  for(i = 0; i < LeftSize; i++){
     L[i] = arr[start + i];
+  }
 
-  for(j = 0; j < RightSize; ++j)
-    R[j] = arr[middle + start + 1];
+  for(j = 0; j < RightSize; j++){
+    R[j] = arr[middle + 1 + j];
+  }
 
+  i = 0;
+  j = 0;
+  k = start;
+  while(i < LeftSize && j < RightSize){
+    if(L[i] <= R[j]){
+      arr[k] = L[i];
+      i++;
+    }else{
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+  while (i < LeftSize){
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+  while(j < RightSize){
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+//  printf("merge log: %i %i %i %i %i %i %i %i\n", start, middle, end, i, j, k, LeftSize, RightSize);
 }
 
 void sort(int arr[], int start, int end){
   if(start < end){
     // if array length is 6
-    // start = 0, end = 5, middle = 3
-    int middle = ((end - start) / 2) + 1;
+    // start = 0, end = 5, middle = 2
+    int middle = start + (end - start) / 2;
 
-    sort(arr, start, middle); // Left side, 0, 3
-    sort(arr, middle + 1, end); // Right side, 4, 5
+    sort(arr, start, middle); // Left side, 0, 2
+    sort(arr, middle + 1, end); // Right side, 3, 5
 
-    merge(arr, start, middle, end); // 0, 3, 5
+    merge(arr, start, middle, end); // 0, 2, 5
   }
 }
 
@@ -51,24 +77,28 @@ void mergeSort(int size){
     int r = rand() % 10000;
     arr[i] = r;
   }
-
+  //printArray(arr, size);
   sort(arr, 0, size - 1);
+  //printArray(arr, size);
 }
 
 int main(){ 
-  printf("benchmark sorting: %i %i %i %i %i\n", 100, 1000, 5000, 10000, 50000);
-  int arr[5] = {100, 1000, 5000, 10000, 50000};
+  printf("benchmark sorting: %i %i %i %i %i %i\n", 100, 1000, 5000, 10000, 50000, 100000);
+  int arr[6] = {100, 1000, 5000, 10000, 50000, 100000};
 
-  printf("Single core test start \n");
-  for(int i = 0; i < 5; i++){
+
+  //mergeSort(50);
+  //return 0;
+  printf("Single-core test start \n");
+  for(int i = 0; i < 6; i++){
     clock_t starttime = clock();
     mergeSort(arr[i]);
     printf("result: %i,  %f\n", arr[i], ((double)(clock() - starttime)) / CLOCKS_PER_SEC);
   }
 
-  printf("Single core test start \n");
+  printf("Multi-core test start \n");
 
-  for(int i = 0; i < 5; i++){
+  for(int i = 0; i < 6; i++){
     clock_t starttime = clock();
     mergeSort(arr[i]);
     printf("result: %i,  %f\n", arr[i], ((double)(clock() - starttime)) / CLOCKS_PER_SEC);
