a6ceee386e817e5d81ecc5dc74dd4ab715eadd25
ZhuElly <chu.elly@hotmail.com.tw>
2023-11-19 00:25:39 +0800

    Yes

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 mkdir bin
 
 echo basic application
-gcc -std=c11 -Wall -W argument.c -o bin/argument.exe
-gcc -std=c11 -Wall -W -Ilib/tinycthread/source multi_thread.c -o bin/multi_thread.exe
+gcc -std=c11 -Wall -W src/basic/argument.c -o bin/argument.exe
+gcc -std=c11 -Wall -W -Ilib/tinycthread/source src/thread/multi_thread.c -o bin/multi_thread.exe
 
 echo sorting algorithms
-gcc -std=c11 -Wall -W selectSort.c -o bin/selectSort.exe
-gcc -std=c11 -Wall -W mergeSort.c -o bin/mergeSort.exe
-gcc -std=c11 -Wall -W bubbleSort.c -o bin/bubbleSort.exe
-gcc -std=c11 -Wall -W insertSort.c -o bin/insertSort.exe
-gcc -std=c11 -Wall -W quickSort.c -o bin/quickSort.exe
+gcc -std=c11 -Wall -W -Isrc/util src/sorting/selectSort.c -o bin/selectSort.exe
+gcc -std=c11 -Wall -W -Isrc/util src/sorting/mergeSort.c -o bin/mergeSort.exe
+gcc -std=c11 -Wall -W -Isrc/util src/sorting/bubbleSort.c -o bin/bubbleSort.exe
+gcc -std=c11 -Wall -W -Isrc/util src/sorting/insertSort.c -o bin/insertSort.exe
+gcc -std=c11 -Wall -W -Isrc/util src/sorting/quickSort.c -o bin/quickSort.exe
 
 echo search algorithms
---
+++ src/basic/argument.c
@@ -0,0 +1,16 @@
+  ~/DSA_Learning
+   bin
+   lib
+   src
+    󰉖 basic
+     search
+     sorting
+     struct
+     thread
+     util                            
+   │ * baisc
+   └  sorting.c
+    LICENSE
+    README.md
+    build.sh                        󰄱 
+   (2 hidden items)
